44f0988f564ce12c2780762c99d7b88b317efdfc
anya-kashchenko <143807908+anna-klbn@users.noreply.github.com>
2023-10-24 22:16:22 +0300

    start

---
+++ src/js/01-gallery.js
@@ -0,0 +1,5 @@
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRefs = document.querySelector('.gallery');
