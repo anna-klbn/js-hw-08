8007598fc6fd77575c15832dd7538256971fac34
anya-kashchenko <143807908+anna-klbn@users.noreply.github.com>
2023-11-24 19:08:46 +0200

    task 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,2 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items.js';
-import SimpleLightbox from 'simplelightbox';
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardMarkup(galleryItems);
+
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+function createGalleryCardMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  }).join('')
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+
+    captionsData: "alt",
+    captionDelay: 250,
+
+});
+lightbox.on('show.simplelightbox'); 
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,41 @@
-import '../css/common.css';
-import '../css/03-feedback.css';
-import throttle from 'lodash.throttle';
+// import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = 'feedback-form-state';
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form textarea'),
-  input: document.querySelector('input'),
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const textarea = document.querySelector('textarea[name="message"]');
+const FORM_DATA = 'feedback-form-state';
+const currentValues = {
+  email: '',
+  message: '',
 };
-const formData = {};
 
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', onFormInput);
 populateTextarea();
 
-refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+  console.log(currentValues);
+   if (event.currentTarget.email.value === '' || event.currentTarget.message.value === '') {
+    return alert(`Заповніть всі поля.`);
+    }
+  event.currentTarget.reset();
+  localStorage.removeItem(FORM_DATA); 
+}
 
-refs.form.addEventListener('submit', e => {
-  e.preventDefault();
-  e.currentTarget.reset();
-  const objData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  localStorage.removeItem(STORAGE_KEY);
-});
-
-function onTextareaInput(e) {
-  formData[e.target.name] = e.target.value;
-  const stringifiedData = JSON.stringify(formData);
-  localStorage.setItem(STORAGE_KEY, stringifiedData);
+function onFormInput(event) {
+  currentValues.email = event.currentTarget.email.value;
+  currentValues.message = event.currentTarget.message.value;
+  
+    localStorage.setItem(FORM_DATA, JSON.stringify(currentValues));
 }
 
 function populateTextarea() {
-  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-  if (savedMessage === null) {
-    //console.log(savedMessage);
-    return;
-  }
-  refs.textarea.value = savedMessage['message'] || '';
-  refs.input.value = savedMessage['email'] || '';
+  const savedValues = JSON.parse(localStorage.getItem(FORM_DATA));
+  
+    if (savedValues) {
+      emailInput.value = savedValues.email;
+      textarea.value = savedValues.message;
+    }
 }
